117f11b1fd4cf3503b89cfa61b4204241fb34ccd
Sasha Zin <zinalbo@tut.by>
2023-07-07 20:53:50 +0300

    add APODTest

---
+++ src/test/java/APODTest.java
@@ -0,0 +1,20 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class APODTest {
+
+    private String URL = "https://api.nasa.gov/planetary/";
+
+    @Test
+    public void checkMediaTypeIsImage() {
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL + "apod?api_key=YcxLwjGaFVxYegEk963AY1HtiDewWK7poVPllKKp&date=2023-07-07")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .and().body("media_type", Matchers.is("image"));
+    }
+}
